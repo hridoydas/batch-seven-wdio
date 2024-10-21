0ed054d440052d8ec9072629982e7cbec448b940
hridoy-foxtel <hridoy.das@foxtel.com.au>
2024-10-21 21:15:57 +0600

    added log

--- wdio.conf.js
+++ wdio.conf.js
@@ -55,8 +55,10 @@ exports.config = {
   //
   capabilities: [
     {
-      // capabilities for local browser web tests
-      browserName: "chrome", // or "firefox", "microsoftedge", "safari"
+      browserName: "chrome",
+      "goog:chromeOptions": {
+        args: ["--disable-extensions"],
+      },
     },
   ],
 
@@ -67,7 +69,7 @@ exports.config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: "silent",
+  logLevel: "error",
   //
   // Set specific log levels per logger
   // loggers:
@@ -107,7 +109,16 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  // services: [],
+  services: [
+    [
+      "chromedriver",
+      {
+        logFileName: "wdio-chromedriver.log", // default
+        outputDir: "driver-logs", // overwrites the config.outputDir
+        args: ["--silent"],
+      },
+    ],
+  ],
   //
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
